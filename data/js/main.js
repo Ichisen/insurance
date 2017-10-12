10a546a900617e9ef1b3a14742ce26bcf55dee15
Ichisen <alfironius@gmail.com>
2017-10-12 14:21:13 +0600

    enifin
    enifing1

--- data/js/main.js
+++ data/js/main.js
@@ -12,3 +12,13 @@ $('.main-message-tab').click(function(elem){
 	$(elem.currentTarget).addClass('active-tab');
 	$('.left-content-tab[data-tab-name='+tabName+']').addClass('active-content-tab');
 });
+
+$('.services-cell').hover( function(event){
+	var $cell = $(event.currentTarget);
+	$cell.find('.services-cell-preview').fadeOut( 500 );
+	$cell.find('.services-cell-review').fadeIn( 500 );
+}, function(event){
+	var $cell = $(event.currentTarget);
+	$cell.find('.services-cell-review').fadeOut( 500 );
+	$cell.find('.services-cell-preview').fadeIn( 500 );
+} );
